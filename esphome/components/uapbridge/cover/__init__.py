022e1ba4e41414315d7463b3f6174a84556a0308
Emanuel <66078016+moosbewohner@users.noreply.github.com>
2026-01-02 12:05:55 +0100

    Update __init__.py

--- esphome/components/uapbridge/cover/__init__.py
+++ esphome/components/uapbridge/cover/__init__.py
@@ -2,18 +2,26 @@ from esphome.components import cover
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from .. import uapbridge_ns, CONF_UAPBRIDGE_ID, UAPBridge
-from esphome.const import (
-  CONF_ID
-)
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uapbridge"]
 
 UAPBridgeCover = uapbridge_ns.class_("UAPBridgeCover", cover.Cover, cg.Component)
 
-CONFIG_SCHEMA = (
-   cover.COVER_SCHEMA.extend(
+# ESPHome >= 2025.11: cover.cover_schema(...)
+# ESPHome <  2025.11: cover.COVER_SCHEMA
+if hasattr(cover, "cover_schema"):
+    BASE_SCHEMA = cover.cover_schema(UAPBridgeCover)
+else:
+    BASE_SCHEMA = cover.COVER_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(UAPBridgeCover),
+        }
+    )
+
+CONFIG_SCHEMA = (
+    BASE_SCHEMA.extend(
+        {
             cv.GenerateID(CONF_UAPBRIDGE_ID): cv.use_id(UAPBridge),
         }
     )
@@ -26,4 +34,3 @@ async def to_code(config):
     await cover.register_cover(var, config)
     parent = await cg.get_variable(config[CONF_UAPBRIDGE_ID])
     cg.add(var.set_uapbridge_parent(parent))
-
